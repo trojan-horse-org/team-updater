e71c3eee911e40a3337b77814c0d41a06290d3db
Gus SS <gusshawstewart@github.com>
2023-03-30 16:21:47 +0100

    updated to work with prs

--- .github/scripts/update_teams_from_txt.rb
+++ .github/scripts/update_teams_from_txt.rb
@@ -1,38 +1,37 @@
 require 'octokit'
-require 'pathname'
-
 
 def main
+  github_token = ENV["GITHUB_TOKEN"]
+  repository = ENV["REPOSITORY"]
+  modified_files = ARGV[0].split
 
-print ENV["MODIFIED_FILES"]
-print ENV["ALL_MODIFIED_OR_CREATED_FILE_NAMES"]
-github_token = ENV["GITHUB_TOKEN"]
-modified_txt_files = ENV["MODIFIED_FILES"].split( ' ' ).select {| file | file.end_with?('.txt') }
-
-  client = Octokit:: Client.new(access_token: github_token)
+  client = Octokit::Client.new(access_token: github_token)
   repo = client.repository(repository)
   org = client.organization(repo.owner.login)
 
-  modified_txt_files.each do | modified_file|
-  team_name = File.basename(modified_file, '.txt')
+  modified_files.each do |modified_file|
+    next unless File.extname(modified_file) == '.txt'
 
-  usernames = File.readlines(modified_file).map( & : strip)
+    team_name = File.basename(modified_file, '.txt')
 
-    team = client.org_teams(org.login).find {| t | t.name == team_name }
+    usernames = File.readlines(modified_file).map(&:strip)
+
+    team = client.org_teams(org.login).find { |t| t.name == team_name }
 
     if team.nil?
-    team = client.create_team(org.login, name: team_name, privacy: 'closed')
-    client.add_team_repository(team.id, repository)
+      team = client.create_team(org.login, name: team_name, privacy: 'closed')
+      client.add_team_repository(team.id, repository)
     end
 
-    current_members = team.team_members(team.id).map( & : login)
-    current_members.each {| member | client.remove_team_member(team.id, member) }
-    usernames.each {| username | client.add_team_member(team.id, username) }
+    current_members = client.team_members(team.id).map(&:login)
+    members_to_remove = current_members - usernames
+    members_to_add = usernames - current_members
 
-    puts "Team '#{team_name}' updated with #{usernames.size} members."
+    members_to_remove.each { |member| client.remove_team_member(team.id, member) }
+    members_to_add.each { |username| client.add_team_member(team.id, username) }
+
+    puts "Team '#{team_name}' updated: #{members_to_add.size} members added, #{members_to_remove.size} members removed."
   end
-
-
 end
 
 main if __FILE__ == $PROGRAM_NAME
